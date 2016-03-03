6b0b45e9e2c9fd11392ab248a36feb9c779fca9a
alvarofidalgo <dmj20@hotmail.com>
2016-03-03 21:27:12 +0100

    solve compiled errors

---
+++ numbers/src/main/java/kata/crazy/CrazyNumbers.java
@@ -0,0 +1,9 @@
+package kata.crazy;
+
+
+public class CrazyNumbers {
+
+    public int result() {
+        return 5;
+    }
+}
--- numbers/src/test/java/kata/crazy/CrazyNumbersTest.java
+++ numbers/src/test/java/kata/crazy/CrazyNumbersTest.java
@@ -3,11 +3,18 @@ package kata.crazy;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class CrazyNumbersTest {
+
+
+
 
     @Test
     public void whenParameterIsTenThenDivByTwo(){
-
+        CrazyNumbers crazynumbers = new CrazyNumbers();
+        assertThat(crazynumbers.result(),is(5));
     }
 
 
